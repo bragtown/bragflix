e138eda1d5cf83ba62ae0937547da20cb445b8be
Jordan Braginton <jordan.braginton@gmail.com>
2018-05-02 11:21:25 -0600

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{ swiperType }}</h1>
+    <HomePage/>
+  </div>  
+</template>
+
+<script>
+import HomePage from './HomePage'
+export default {
+  name: 'HelloWorld',
+  components: {
+    HomePage
+  },
+  data () {
+    return {
+      swiperType : 'Easy Vertical Swiper'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  h1{
+    color: #ffffff;
+}
+</style>
